83d69c87d028cb3b8bae6c3ce876afd48075fc36
Jared Baur <jaredbaur@fastmail.com>
2021-10-06 17:08:04 -0700

    Include script for running latexmk on changes

--- shell.nix
+++ shell.nix
@@ -1,6 +1,15 @@
 { pkgs ? import <nixpkgs> { } }:
-pkgs.mkShell {
+let
+  run = pkgs.writeShellScriptBin "run" ''
+    ${pkgs.fd}/bin/fd -e tex | ${pkgs.entr}/bin/entr -c \
+      ${pkgs.texlive.combined.scheme-medium}/bin/latexmk -pdf resume.tex
+  '';
+in
+pkgs.mkShell
+{
   nativeBuildInputs = with pkgs; [
+    entr
+    fd
     texlive.combined.scheme-medium
-  ];
+  ] ++ [ run ];
 }

089d7a2da529fc77cbeb809e7a272752ed5baf04
Jared Baur <jaredbaur@fastmail.com>
2025-04-30 12:38:56 -0700

    Update inputs

--- flake.nix
+++ flake.nix
@@ -1,40 +1,54 @@
 {
-  description = "resume";
-  inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
-  outputs = inputs:
+  description = "jmbaur-resume";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  outputs =
+    inputs:
     let
-      forEachSystem = f: inputs.nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-linux" ] (system: f {
-        inherit system;
-        pkgs = import inputs.nixpkgs {
-          inherit system;
-          overlays = [
-            (final: prev: {
-              tex = prev.texlive.combine {
-                inherit (prev.texlive) scheme-basic latexmk
-                  preprint marvosym titlesec/*verbatim*/ enumitem fancyhdr
-                  ;
-              };
-            })
-          ];
-        };
-      });
+      forEachSystem =
+        f:
+        inputs.nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-linux" ] (
+          system:
+          f (
+            import inputs.nixpkgs {
+              inherit system;
+              overlays = [
+                (final: prev: {
+                  tex = final.texlive.combine {
+                    inherit (final.texlive)
+                      scheme-basic
+                      latexmk
+                      preprint
+                      marvosym
+                      titlesec # verbatim
+                      enumitem
+                      fancyhdr
+                      ;
+                  };
+                })
+              ];
+            }
+          )
+        );
     in
     {
-      packages = forEachSystem ({ pkgs, ... }: {
-        default = pkgs.runCommand "resume-2022-01-22"
-          { src = ./.; } ''
-          mkdir -p $out
-          ${pkgs.tex}/bin/latexmk -pdf $src/resume.tex
-          cp resume.pdf $out
-        '';
+      packages = forEachSystem (pkgs: {
+        default = pkgs.callPackage (
+          { runCommand, tex }:
+          runCommand "jmbaur-resume" { nativeBuildInputs = [ tex ]; } ''
+            latexmk -pdf ${./resume.tex}
+            install -Dm0644 *.pdf $out/resume.pdf
+          ''
+        ) { };
       });
-      apps = forEachSystem ({ pkgs, ... }: {
+      apps = forEachSystem (pkgs: {
         default = {
           type = "app";
-          program = toString (pkgs.writeShellScript "run" ''
-            ${pkgs.fd}/bin/fd -e tex | ${pkgs.entr}/bin/entr -c \
-              ${pkgs.tex}/bin/latexmk -pdf resume.tex
-          '');
+          program = toString (
+            pkgs.writeShellScript "run" ''
+              ${pkgs.fd}/bin/fd -e tex | ${pkgs.entr}/bin/entr -c \
+                ${pkgs.tex}/bin/latexmk -pdf resume.tex
+            ''
+          );
         };
       });
     };
